2d4725dd56d1d906083a9c15e7b2810be502f09f
Mark Lee <markcmlee@gmail.com>
2021-05-01 18:58:53 -0400

    Beanstalk generator now makes leaves with collision

--- Project 7 That's Life 2/World.gd
+++ Project 7 That's Life 2/World.gd
@@ -1,20 +1,19 @@
 extends Node2D
 
 var Beanstalk = load("res://Beanstalk.tscn")
+var LeftLeaf = load("res://LeftLeaf.tscn")
+var RightLeaf = load("res://RightLeaf.tscn")
 
 
 
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Get size of window
 	var windowSize = get_viewport_rect().size
-	print(windowSize)
-
 	var windowX = windowSize.x
-
-	var oneX = windowX / 2
-	var threeX = windowX / 16
+	# Incorrect ratio, but assigns x position to each beanstalk
+	# TOO LAZY TO DO THE PRECISE MATH, SOMEONE DO PLS
+	var oneX = windowX / 2 + 700
+	var threeX = windowX / 2 + 100
 	var twoX = windowX / 32
 
 	for n in range(-5400, 5400, 540):
@@ -44,20 +43,33 @@ func createBeanstalk(posX, posY):
 	beanstalkInstance.global_position.y = posY
 	add_child(beanstalkInstance)
 	
-#	if beanstalkInstance.position.y <= get_viewport_rect().size.y:
-#		createBeanstalk(posX, posY + 540)
+	# adds a left or right leaf with 50% chance each
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var randomNum = int(round(rng.randf_range(0, 1)))
+	if randomNum == 1:
+		createLeftLeaf(posX - 145, posY / 2)
+	else:
+		createRightLeaf(posX + 150, posY / 2)
 
+func createLeftLeaf(leafX, leafY):
+	var leftLeafInstance = LeftLeaf.instance()
+	var scale = Vector2(4, 4)
+	leftLeafInstance.set_scale(scale)
+	leftLeafInstance.set_name("leftLeaf")
+	leftLeafInstance.global_position.x = leafX
+	leftLeafInstance.global_position.y = leafY
+	add_child(leftLeafInstance)
+	
+func createRightLeaf(leafX, leafY):
+	var rightLeafInstance = RightLeaf.instance()
+	var scale = Vector2(4, 4)
+	rightLeafInstance.set_scale(scale)
+	rightLeafInstance.set_name("rightLeaf")
+	rightLeafInstance.global_position.x = leafX
+	rightLeafInstance.global_position.y = leafY
+	add_child(rightLeafInstance)
 
-
-
-
-
-
-
-
-
-func _exit_tree():
-	Global.world = null
 
 
 
@@ -68,6 +80,3 @@ func _exit_tree():
 #func _process(delta):
 #	pass
 
-#func createBeanstalk():
-#	var beanstalk = Beanstalk.instance()
-#	while beanstalk.position.y < OS.get_windows_size():
