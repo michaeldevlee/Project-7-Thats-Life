a29816d3a4bec88ac49689f7920718133761c70d
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2021-05-01 20:31:09 -0700

    updated movement and anims

--- Project 7 That's Life 2/Player.gd
+++ Project 7 That's Life 2/Player.gd
@@ -1,21 +1,36 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-var gravity = Vector2()
+onready var anim = $Player/AnimationPlayer
+onready var sprite = $Player
 
 func _ready():
 	pass # Replace with function body.
 
 
 func handleMovement():
-	velocity = Vector2()
+
 	if(Input.is_action_pressed("ui_right")):
-		velocity.x += 100
-	if(Input.is_action_pressed("ui_left")):
-		velocity.x -= 100
+		velocity.x = 200
+		sprite.flip_h = true
+		anim.play("Walk")
+	elif(Input.is_action_pressed("ui_left")):
+		velocity.x = -200
+		sprite.flip_h = false
+		anim.play("Walk")
+	else:
+		velocity.x = 0
+		anim.play("Idle")
+		
+	
+	if(Input.is_action_just_pressed("ui_up")):
+		velocity.y = -200
+	
 	
 
 func _physics_process(delta):
-	gravity.y *= 100
+	velocity.y = 400
 	handleMovement()
 	velocity = move_and_slide(velocity)
+
+	
